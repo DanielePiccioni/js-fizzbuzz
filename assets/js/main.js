c58c8754c365318a4a0b8dc819da950e60f51973
DanielePiccioni <danielepiccioni1@gmail.com>
2025-10-07 14:41:31 +0200

    write FIzz when the number are multiples of 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,6 +37,9 @@ for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
         console.log("FizzBuzz");
 
+    } else if (i % 3 === 0){
+        console.log("Fizz");
+        
     }
 }
 // fizz se divisibile per 3
