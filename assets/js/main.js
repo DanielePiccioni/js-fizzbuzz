c5c88513db31dcad78989b0c3451f6ad503d4de7
DanielePiccioni <danielepiccioni1@gmail.com>
2025-10-07 13:20:00 +0200

    first step see if number are divisible for 3 or 5

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+/* 
+Ciao ragazzi,
+Esercizio di oggi: FizzBuzz
+nome repo: js-fizzbuzz
+
+Consegna:
+Scrivi un programma che stampi i numeri da 1 a 100,
+ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+Prima di partire a scrivere codice poniamoci qualche domanda:
+1-Come faccio a sapere se un numero è divisibile per?
+2-Abbiamo visto qualcosa di particolare che possiamo usare?
+
+Consigli del giorno:
+scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+*/
+
+// 1- proviamo a usare il modulo "%" per vedere se è divisibile per altri numeri
+// 12 % 3 === 0
+// 20 % 5 === 0
