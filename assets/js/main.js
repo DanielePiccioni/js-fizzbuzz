d1a3c6717444afad3e6da88e364070a99351fb37
DanielePiccioni <danielepiccioni1@gmail.com>
2025-10-07 14:42:38 +0200

    write Buzz when the number are multiples of 5

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,6 +40,9 @@ for (let i = 1; i <= 100; i++) {
     } else if (i % 3 === 0){
         console.log("Fizz");
         
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+        
     }
 }
 // fizz se divisibile per 3
